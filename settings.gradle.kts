e89f0bbb0722b8aef9e79112d47b103e4e531447
ifox6677 <zhangjq0908@outlook.com>
2025-12-20 20:30:59 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
 pluginManagement {
     repositories {
         // 阿里云镜像
+		mavenCentral()
+    
+        // 保留Google仓库(备选)
+        google()
         maven ("https://maven.aliyun.com/repository/public")
         maven ("https://maven.aliyun.com/repository/google")
         maven ("https://maven.aliyun.com/repository/gradle-plugin")
@@ -18,10 +22,7 @@ pluginManagement {
         
     
         // 保留中央仓库
-        mavenCentral()
-    
-        // 保留Google仓库(备选)
-        google()
+
     }
 }
 dependencyResolutionManagement {
@@ -29,14 +30,15 @@ dependencyResolutionManagement {
 
     repositories {
         // ======== 官方必须优先 ========
-        google()         // 必须第一个
-        mavenCentral()   // 其次
+
 
         // ======== 国内镜像（辅助加速） ========
         maven("https://mirrors.tencent.com/nexus/repository/maven-public/")
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/public")
         maven("https://mirrors.huaweicloud.com/repository/maven/")
+		google()         // 必须第一个
+        mavenCentral()   // 其次
 
         // ======== JitPack（特殊依赖） ========
         maven("https://jitpack.io")
