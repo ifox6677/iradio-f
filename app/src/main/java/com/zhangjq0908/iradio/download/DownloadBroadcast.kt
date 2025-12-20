67ef7b02a287a62e53479501223aad6c7cb55499
ifox6677 <zhangjq0908@outlook.com>
2025-12-20 20:26:50 +0800

    Create DownloadBroadcast.kt

---
+++ app/src/main/java/com/zhangjq0908/iradio/download/DownloadBroadcast.kt
@@ -0,0 +1,13 @@
+package com.zhangjq0908.iradio.download
+
+object DownloadBroadcast {
+
+    const val ACTION_DOWNLOAD_FINISHED =
+        "com.example.iradio.DOWNLOAD_FINISHED"
+    		
+
+    const val EXTRA_EPISODE_URL = "episode_url"
+	const val EXTRA_EPISODE_TITLE = "episode_title"
+    const val EXTRA_LOCAL_PATH = "local_path"
+
+}
